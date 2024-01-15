6cbeb45665aff23e34a98e021c9c84e9d306385a
DanGarcia963 <de.espejelg@gmail.com>
2024-01-15 12:20:24 -0600

    Index

--- assets/js/formulario.js
+++ assets/js/formulario.js
@@ -6,5 +6,5 @@ btn_sub.addEventListener('click',(e)=>{
     let genero=parseFloat(form['genero'].value),edad=parseFloat(form['edad'].value);
     let estado=parseFloat(form['estado'].value),comorbilidades=parseFloat(form['comorbilidades'].value);
     let res=genero*edad*estado*comorbilidades*100;
-    re.innerHTML='<div class="d-flex justify-content-center"><p class="text-uppercase font-weight-bold">Tu proabilidad de morir por covid es:'+res+'%</p></div>';
+    re.innerHTML='<div class="d-flex justify-content-center"><p class="text-uppercase font-weight-bold">Tu probabilidad de morir por covid es: '+res+'%</p></div>';
 })
