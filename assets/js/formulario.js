53a6248d2efb285c77ca6a288b066c5575c8680c
Angel788 <angel-yahel@outlook.com>
2024-01-15 03:19:23 -0600

    Funcionalidad

---
+++ assets/js/formulario.js
@@ -0,0 +1,10 @@
+let form=document.getElementById('formulario');
+let re=document.getElementById('res');
+let btn_sub=document.getElementById('btn-sub');
+btn_sub.addEventListener('click',(e)=>{
+    e.preventDefault();
+    let genero=parseFloat(form['genero'].value),edad=parseFloat(form['edad'].value);
+    let estado=parseFloat(form['estado'].value),comorbilidades=parseFloat(form['comorbilidades'].value);
+    let res=genero*edad*estado*comorbilidades*100;
+    re.innerHTML='<div class="d-flex justify-content-center"><p class="text-uppercase font-weight-bold">Tu proabilidad de morir por covid es:'+res+'%</p></div>';
+})
